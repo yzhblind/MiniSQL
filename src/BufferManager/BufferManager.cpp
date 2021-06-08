b85ad6be94fbc9fd43e5c33a233f2a138f34dc62
杨子晗 <yangzihan2000@outlook.com>
2021-06-08 18:23:22 +0800

    BufferManager Interface v1

--- src/BufferManager/BufferManager.cpp
+++ src/BufferManager/BufferManager.cpp
@@ -1,1 +1,21 @@
 #include "BufferManager.hpp"
+
+const int BufferManager::pageSize = 4096;
+int BufferManager::getPageSize() { return pageSize; }
+const int BufferManager::bufferSize = 4096;
+int BufferManager::getBufferSize() { return bufferSize; }
+
+BufferManager::node::node(dword virtAddr, void *phyAddr, int size) : virtAddr(virtAddr), phyAddr(phyAddr), size(size) {}
+hword BufferManager::node::getFileAddr() const { return virtAddr >> 48; }
+hword BufferManager::node::getOffset() const { return virtAddr & 0xFFFF; }
+word BufferManager::node::getBlockAddr() const { return (virtAddr >> 16) & 0xFFFFFFFF; }
+dword BufferManager::node::getVirtAddr() const { return virtAddr; }
+
+BufferManager::BufferManager()
+{
+}
+BufferManager::~BufferManager()
+{
+}
+
+BufferManager bufMgr;
--- src/Include/BufferManager.hpp
+++ src/Include/BufferManager.hpp
@@ -1,1 +1,87 @@
 #pragma once
+
+#include <list>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+typedef unsigned short int hword;
+typedef unsigned int word;
+typedef unsigned long long dword;
+
+class BufferManager
+{
+public:
+    enum fileType
+    {
+        DATA,
+        OTHERS
+    };
+    enum pinType
+    {
+        PIN,
+        UNPIN
+    };
+    class node
+    {
+    private:
+        dword virtAddr;
+        void *phyAddr;
+        int size;
+
+    public:
+        node(dword virtAddr, void *phyAddr, int size);
+        bool isValid() const;
+        hword getFileAddr() const;
+        hword getOffset() const;
+        word getBlockAddr() const;
+        dword getVirtAddr() const;
+        int read(const void *const dest, const int offset, const int byteSize) const;
+        int write(void *const src, const int offset, const int byteSize);
+    };
+
+private:
+    struct buffer
+    {
+        std::vector<bool> bufferValid;
+        std::vector<bool> bufferDirty;
+        std::vector<int> bufferPinned;
+        std::vector<dword> bufferTag;
+        std::unordered_map<dword, int> tagIndex;
+        std::vector<void *> bufferPool;
+        std::list<int> LRU, fixed;
+        std::vector<std::list<int>::iterator> LRUIndex;
+    };
+    struct fileIndex
+    {
+        std::vector<FILE *> pointer;
+        std::vector<fileType> type;
+        std::vector<int> blockNum, recordSize, nextFree;
+    };
+
+    buffer buf;
+    fileIndex index;
+
+    static const int bufferSize;
+    static const int pageSize;
+
+    int replaceBlock(const hword fileAddr, const word blockAddr);
+
+public:
+    static inline int getPageSize();
+    static inline int getBufferSize();
+    BufferManager();
+    ~BufferManager();
+    int init();
+    int openFile(const std::string filename, const fileType type);
+    node getNextFree(const hword fileAddr);
+    node getRootBlock(const hword fileAddr);
+    node setRootBlock(const hword fileAddr, const word blockAddr);
+    node getBlock(const hword fileAddr, const word blockAddr);
+    int deleteBlock(const hword fileAddr, const word blockAddr);
+    int pinBlock(const hword fileAddr, const word blockAddr, const pinType type);
+    int deleteRecord(const hword fileAddr, const word blockAddr, const hword offset);
+};
+
+extern BufferManager bufMgr;
