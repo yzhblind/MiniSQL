fa9705c0a51f442b1a880fe2c8c0ff1052d8f901
杨子晗 <yangzihan2000@outlook.com>
2021-06-13 13:52:40 +0800

    test pin and unpin

--- src/testBM.cpp
+++ src/testBM.cpp
@@ -7,9 +7,13 @@ int main()
     int fileAddr = bufMgr.openFile("testRecord.db", DATA, 25);
     for (int i = 0; i < 64; ++i)
     {
+        if (i == 12)
+            bufMgr.pinBlock(fileAddr, 11, PIN);
         string str = "123456789!";
         node dest = bufMgr.getNextFree(fileAddr);
         dest.write(str.c_str(), 0, str.size() + 1);
+        if (i == 21)
+            bufMgr.pinBlock(fileAddr, 11, UNPIN);
     }
     // bufMgr.deleteRecord(fileAddr, 3, 14);
     // bufMgr.deleteRecord(fileAddr, 4, 21);
