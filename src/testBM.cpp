9495369c2435a0af804c22c774f8ae7ccacb7984
杨子晗 <yangzihan2000@outlook.com>
2021-06-13 13:27:48 +0800

    test LRU method

--- src/testBM.cpp
+++ src/testBM.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-    int fileAddr = bufMgr.openFile("testRecord.db", DATA, 6);
-    for (int i = 5; i < 9; ++i)
+    int fileAddr = bufMgr.openFile("testRecord.db", DATA, 25);
+    for (int i = 0; i < 64; ++i)
     {
-        string str = std::to_string(i + i * 10 + i * 100 + i * 1000);
+        string str = "123456789!";
         node dest = bufMgr.getNextFree(fileAddr);
         dest.write(str.c_str(), 0, str.size() + 1);
     }
-    bufMgr.deleteRecord(fileAddr, 3, 14);
-    bufMgr.deleteRecord(fileAddr, 4, 21);
-    node src = bufMgr.getRecord(fileAddr, 6, 7);
-    char s[256];
-    src.read(s, 0, 6);
-    cout << string(s) << endl;
+    // bufMgr.deleteRecord(fileAddr, 3, 14);
+    // bufMgr.deleteRecord(fileAddr, 4, 21);
+    // node src = bufMgr.getRecord(fileAddr, 6, 7);
+    // char s[256];
+    // src.read(s, 0, 6);
+    // cout << string(s) << endl;
     return 0;
 }
