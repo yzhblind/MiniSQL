8eb804ffe1ddc162720bf7e51892fcbd08143bfb
杨子晗 <yangzihan2000@outlook.com>
2021-06-15 18:50:27 +0800

    init struct attribute and schema

--- src/Include/Type.hpp
+++ src/Include/Type.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+struct attribute
+{
+    // 0:int
+    // 1:float
+    // 2-256:char(2-256) [包含'\0']
+    int type;
+    // 0表示NULL
+    int indexRootAddr;
+    // false表示不唯一，true表示唯一
+    bool isUnique;
+};
+struct schema
+{
+    std::string tableName;
+    std::map<std::string, int> attributeIndex;
+    std::vector<attribute> column;
+    int primaryKey;
+    int recordSize;
+};
