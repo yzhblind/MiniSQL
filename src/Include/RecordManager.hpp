4ec8b39c6ab0cafbc98324bab1460c2587d44245
杨子晗 <yangzihan2000@outlook.com>
2021-06-15 22:03:59 +0800

    nothing

--- src/Include/RecordManager.hpp
+++ src/Include/RecordManager.hpp
@@ -21,11 +21,16 @@ struct condExpr
     static const void *copyVal(const std::vector<attribute> &origin, const int pos, const void *val);
     condExpr(const std::vector<attribute> &origin, const int pos, const op opr, const void *val) : origin(origin), pos(pos), opr(opr), val(copyVal(origin, pos, val)) {}
     ~condExpr() { delete[] static_cast<const char *>(val); }
+    condExpr(const condExpr &obj) : origin(obj.origin), pos(obj.pos), opr(obj.opr), val(copyVal(obj.origin, obj.pos, obj.val)) {}
 };
-
 class filter
 {
-
+private:
+    const std::vector<attribute> &origin;
+    std::vector<condExpr> cond;
+    std::vector<void *> res;
+public:
+    
 };
 
 class RecordManager
