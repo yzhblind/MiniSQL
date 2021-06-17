9b8172f71ba215ed6d19f428cfe6e7834e8a44e7
杨子晗 <yangzihan2000@outlook.com>
2021-06-17 10:20:17 +0800

    test record manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include "Interpreter.hpp"
+#include "RecordManager.hpp"
+using namespace std;
 
 int main()
 {
+    int fileAddr = bufMgr.openFile("test.db", DATA, 2);
+    vector<attribute> column;
+    column.resize(1);
+    column[0].type = 2;
+    char str[2];
+    str[2] = '\0';
+    for (int i = 'A'; i <= 'Z'; ++i)
+    {
+        str[0] = i;
+        rcdMgr.insertRecord(fileAddr, str);
+    }
+    filter flt(column);
+    char a[2] = "F";
+    char b[2] = "X";
+    flt.addCond(condExpr(column, 0, GE, a));
+    flt.addCond(condExpr(column, 0, L, b));
+    rcdMgr.deleteRecord(fileAddr, flt);
+    //rcdMgr.selectRecord(fileAddr, flt);
+    //for (int i = 0; i < flt.res.size(); ++i)
+    //    puts(static_cast<char *>(flt.res[i]));
     return 0;
 }
