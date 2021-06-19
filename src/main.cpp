0aaa62b4dbe5b832ae841accfd4b1e6bd1b197f0
C-ld <57717426+C-ld@users.noreply.github.com>
2021-06-20 05:39:54 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-#include "Interpreter.hpp"
+#include "interpreter.hpp"
 
-int main()
-{
-    return 0;
+
+int main() {
+	inst_reader in;
+	while (in.translate() != quit) {
+		in.clear();
+		in.reread();
+	}
+	return 0;
 }
