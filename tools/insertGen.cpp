879856cee625ee7b07584afdcd546a7670c3042a
杨子晗 <yangzihan2000@outlook.com>
2021-07-01 00:16:16 +0800

    modify tools

---
+++ tools/insertGen.cpp
@@ -0,0 +1,17 @@
+#include<set>
+#include<vector>
+#include<string>
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+string random_string_gen(int len)
+{
+    char s[len];
+}
+
+int main()
+{
+    return 0;
+}
